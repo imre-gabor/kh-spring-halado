24ab0eaf55958649120850db5d6ad25940214a81
imre-gabor <gabor@aut.bme.hu>
2024-01-24 10:41:05 +0100

    add config server and use it from microservices (token, flights, currency)

---
+++ config/src/test/java/com/khb/hu/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khb.hu.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
