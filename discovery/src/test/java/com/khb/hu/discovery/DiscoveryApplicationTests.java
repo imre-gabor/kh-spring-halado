2f5fc663c4a3060cf54e3131f19012ec21e8ee3c
imre-gabor <gabor@aut.bme.hu>
2024-01-24 12:11:03 +0100

    add discovery server, and its yml at config server

---
+++ discovery/src/test/java/com/khb/hu/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khb.hu.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
